30cea7b785d41a39d284893676e2122e18316a9f
liuyu <liuyu@10.38.15.131>
2015-10-07 11:17:21 -0400

    Liu Yu Homework3

--- EN234_FEA/user_codes/src/user_element.f90
+++ EN234_FEA/user_codes/src/user_element.f90
@@ -64,6 +64,13 @@ subroutine user_element_static(lmn, element_identifier, n_nodes, node_property_l
     n_state_variables, initial_state_variables, &                                                ! Input variables
     updated_state_variables,element_stiffness,element_residual, fail)      ! Output variables
 
+    else if ( element_identifier == 101) then           ! Stub for a new element
+
+        call el_linelast_2dbasic(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+    n_properties, element_properties, element_coords, length_coord_array, &                      ! Input variables
+    dof_increment, dof_total, length_dof_array, &                                                ! Input variables
+    n_state_variables, initial_state_variables, &                                                ! Input variables
+    updated_state_variables,element_stiffness,element_residual, fail)
 
     else if ( element_identifier ==0) then           ! Stub for a new element
   
@@ -223,6 +230,15 @@ subroutine user_element_fieldvariables(lmn, element_identifier, n_nodes, node_pr
             n_state_variables, initial_state_variables,updated_state_variables, &                       ! Input variables
             n_field_variables,field_variable_names, &                                                   ! Field variable definition
             nodal_fieldvariables)      ! Output variables
+
+        else if ( element_identifier == 101 ) then
+            call fieldvars_linelast_2dbasic(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
+                n_properties, element_properties,element_coords, length_coord_array, &                                   ! Input variables
+                dof_increment, dof_total, length_dof_array, &                                                            ! Input variables
+                n_state_variables, initial_state_variables,updated_state_variables, &                                    ! Input variables
+                n_field_variables,field_variable_names, &                                                                ! Field variable definition
+                nodal_fieldvariables)      ! Output variables
+
 
         else if ( element_identifier == 0 ) then
             call new_user_element_fieldvariables(lmn, element_identifier, n_nodes, node_property_list, &           ! Input variables
